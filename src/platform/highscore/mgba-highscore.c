2367537aeda8cb9e311f22db46b68f2b49ef5666
Alice Mikhaylenko <alicem@gnome.org>
2025-06-21 04:56:17 +0400

    highscore: Stop handling GB
    We use SameBoy for that now, so keep it GBA-only.

--- src/platform/highscore/mgba-highscore.c
+++ src/platform/highscore/mgba-highscore.c
@@ -15,7 +15,6 @@
 #include <mgba-util/image.h>
 #include <mgba-util/vfs.h>
 
-#define GB_SAMPLES 512
 #define SAMPLES_PER_FRAME_MOVING_AVG_ALPHA (1.0f / 180.0f)
 
 static mGBACore *core;
@@ -25,7 +24,6 @@ struct _mGBACore
   HsCore parent_instance;
 
   struct mCore *core;
-  struct mAVStream stream;
   HsSoftwareContext *context;
 
   size_t audio_buffer_size;
@@ -35,40 +33,12 @@ struct _mGBACore
   struct mLogger logger;
 
   struct mRumbleIntegrator rumble;
-
-  enum GBModel gb_model;
-  int colorburst_phase;
 };
 
-static void mgba_game_boy_core_init (HsGameBoyCoreInterface *iface);
 static void mgba_game_boy_advance_core_init (HsGameBoyAdvanceCoreInterface *iface);
 
 G_DEFINE_FINAL_TYPE_WITH_CODE (mGBACore, mgba_core, HS_TYPE_CORE,
-                               G_IMPLEMENT_INTERFACE (HS_TYPE_GAME_BOY_CORE, mgba_game_boy_core_init)
                                G_IMPLEMENT_INTERFACE (HS_TYPE_GAME_BOY_ADVANCE_CORE, mgba_game_boy_advance_core_init))
-
-static inline gboolean
-is_gba (mGBACore *self)
-{
-  return hs_core_get_platform (HS_CORE (self)) == HS_PLATFORM_GAME_BOY_ADVANCE;
-}
-
-static inline gboolean
-is_gb (mGBACore *self)
-{
-  return hs_core_get_platform (HS_CORE (self)) == HS_PLATFORM_GAME_BOY;
-}
-
-static inline void
-refresh_screen_area (mGBACore *self)
-{
-  unsigned width, height;
-
-  self->core->currentVideoSize (self->core, &width, &height);
-
-  hs_software_context_set_area (self->context,
-                                &HS_RECTANGLE_INIT (0, 0, width, height));
-}
 
 static void
 log_cb (struct mLogger* logger, int category, enum mLogLevel level, const char* format, va_list args)
@@ -157,8 +127,6 @@ mgba_core_start (HsCore *core)
   mGBACore *self = MGBA_CORE (core);
 
   self->core->reset (self->core);
-
-  refresh_screen_area (self);
 }
 
 static void
@@ -167,8 +135,6 @@ mgba_core_reset (HsCore *core, gboolean hard)
   mGBACore *self = MGBA_CORE (core);
 
   self->core->reset (self->core);
-
-  refresh_screen_area (self);
 }
 
 static void
@@ -179,18 +145,7 @@ mgba_core_stop (HsCore *core)
   g_clear_object (&self->context);
 }
 
-const int GB_BUTTON_MAPPING[] = {
-  GB_KEY_UP,
-  GB_KEY_DOWN,
-  GB_KEY_LEFT,
-  GB_KEY_RIGHT,
-  GB_KEY_A,
-  GB_KEY_B,
-  GB_KEY_SELECT,
-  GB_KEY_START,
-};
-
-const int GBA_BUTTON_MAPPING[] = {
+const int BUTTON_MAPPING[] = {
   GBA_KEY_UP,
   GBA_KEY_DOWN,
   GBA_KEY_LEFT,
@@ -209,21 +164,11 @@ mgba_core_poll_input (HsCore *core, HsInputState *input_state)
   mGBACore *self = MGBA_CORE (core);
 
   uint32_t keys = 0;
+  uint32_t buttons = input_state->game_boy_advance.buttons;
 
-  if (is_gba (self)) {
-    uint32_t buttons = input_state->game_boy_advance.buttons;
-
-    for (int btn = 0; btn < HS_GAME_BOY_ADVANCE_N_BUTTONS; btn++) {
-      if (buttons & 1 << btn)
-        keys |= 1 << GBA_BUTTON_MAPPING[btn];
-    }
-  } else {
-    uint32_t buttons = input_state->game_boy.buttons;
-
-    for (int btn = 0; btn < HS_GAME_BOY_N_BUTTONS; btn++) {
-      if (buttons & 1 << btn)
-        keys |= 1 << GB_BUTTON_MAPPING[btn];
-    }
+  for (int btn = 0; btn < HS_GAME_BOY_ADVANCE_N_BUTTONS; btn++) {
+    if (buttons & 1 << btn)
+      keys |= 1 << BUTTON_MAPPING[btn];
   }
 
   self->core->setKeys (self->core, keys);
@@ -236,44 +181,35 @@ mgba_core_run_frame (HsCore *core)
 
   self->core->runFrame (self->core);
 
-  if (is_gba (self)) {
-    struct mAudioBuffer *buffer = self->core->getAudioBuffer (self->core);
-    int available = mAudioBufferAvailable (buffer);
+  struct mAudioBuffer *buffer = self->core->getAudioBuffer (self->core);
+  int available = mAudioBufferAvailable (buffer);
 
-    if (available > 0) {
-      size_t samples_to_read;
+  if (available > 0) {
+    size_t samples_to_read;
 
-      /* Update 'running average' of number of
-       * samples per frame.
-       * Note that this is not a true running
-       * average, but just a leaky-integrator/
-       * exponential moving average, used because
-       * it is simple and fast (i.e. requires no
-       * window of samples). */
-      self->audio_samples_per_frame_avg =
-        (SAMPLES_PER_FRAME_MOVING_AVG_ALPHA * (float) available) +
-         ((1.0f - SAMPLES_PER_FRAME_MOVING_AVG_ALPHA) * self->audio_samples_per_frame_avg);
+    /* Update 'running average' of number of
+     * samples per frame.
+     * Note that this is not a true running
+     * average, but just a leaky-integrator/
+     * exponential moving average, used because
+     * it is simple and fast (i.e. requires no
+     * window of samples). */
+    self->audio_samples_per_frame_avg =
+      (SAMPLES_PER_FRAME_MOVING_AVG_ALPHA * (float) available) +
+       ((1.0f - SAMPLES_PER_FRAME_MOVING_AVG_ALPHA) * self->audio_samples_per_frame_avg);
 
-      samples_to_read = (size_t) self->audio_samples_per_frame_avg;
+    samples_to_read = (size_t) self->audio_samples_per_frame_avg;
 
-      /* Resize audio output buffer, if required */
-      if (self->audio_buffer_size < samples_to_read * 2) {
-        self->audio_buffer_size = samples_to_read * 2;
-        self->audio_buffer = realloc (self->audio_buffer, self->audio_buffer_size * sizeof(int16_t));
-      }
-
-      int produced = mAudioBufferRead (buffer, self->audio_buffer, samples_to_read);
-
-      if (produced > 0)
-        hs_core_play_samples (core, self->audio_buffer, produced * 2);
+    /* Resize audio output buffer, if required */
+    if (self->audio_buffer_size < samples_to_read * 2) {
+      self->audio_buffer_size = samples_to_read * 2;
+      self->audio_buffer = realloc (self->audio_buffer, self->audio_buffer_size * sizeof(int16_t));
     }
-  } else {
-    if (self->gb_model == GB_MODEL_SGB || self->gb_model == GB_MODEL_SGB2)
-      self->colorburst_phase ^= 1;
-    else
-      self->colorburst_phase = 0;
 
-    hs_software_context_set_colorburst_phase (self->context, self->colorburst_phase);
+    int produced = mAudioBufferRead (buffer, self->audio_buffer, samples_to_read);
+
+    if (produced > 0)
+      hs_core_play_samples (core, self->audio_buffer, produced * 2);
   }
 }
 
@@ -302,8 +238,6 @@ mgba_core_load_state (HsCore          *core,
   mCoreLoadStateNamed (self->core, vf, SAVESTATE_SAVEDATA | SAVESTATE_RTC);
 
   vf->close (vf);
-
-  refresh_screen_area (self);
 
   callback (core, NULL);
 }
@@ -336,16 +270,10 @@ mgba_core_get_aspect_ratio (HsCore *core)
 {
   mGBACore *self = MGBA_CORE (core);
   unsigned width, height;
-  double par;
 
   self->core->currentVideoSize (self->core, &width, &height);
 
-  if (!is_gba (self) && (self->gb_model == GB_MODEL_SGB || self->gb_model == GB_MODEL_SGB2))
-    par = 8.0 / 7.0;
-  else
-    par = 1.0;
-
-  return (double) width / (double) height * par;
+  return (double) width / (double) height;
 }
 
 static double
@@ -356,32 +284,6 @@ mgba_core_get_sample_rate (HsCore *core)
   return self->core->audioSampleRate (self->core);
 }
 
-static HsRegion
-mgba_core_get_region (HsCore *core)
-{
-  mGBACore *self = MGBA_CORE (core);
-
-  if (!is_gba (self) && (self->gb_model == GB_MODEL_SGB || self->gb_model == GB_MODEL_SGB2))
-    return HS_REGION_NTSC;
-
-  return HS_REGION_UNKNOWN;
-}
-
-static void
-postAudioBuffer (struct mAVStream* stream, struct mAudioBuffer* buffer)
-{
-  int produced;
-
-  UNUSED(stream);
-
-  g_assert (core);
-
-  produced = mAudioBufferRead (buffer, core->audio_buffer, GB_SAMPLES);
-
-  if (produced > 0)
-    hs_core_play_samples (HS_CORE (core), core->audio_buffer, produced * 2);
-}
-
 static void
 mgba_core_constructed (GObject *object)
 {
@@ -389,12 +291,7 @@ mgba_core_constructed (GObject *object)
 
   G_OBJECT_CLASS (mgba_core_parent_class)->constructed (object);
 
-  if (is_gba (self))
-    self->core = GBACoreCreate ();
-  else if (is_gb (self))
-    self->core = GBCoreCreate ();
-  else
-    g_assert_not_reached ();
+  self->core = GBACoreCreate ();
 
   mCoreInitConfig (self->core, NULL);
 
@@ -411,36 +308,22 @@ mgba_core_constructed (GObject *object)
 
   self->core->init (self->core);
 
-  if (is_gba (self)) {
-    size_t samples_per_frame;
+  size_t samples_per_frame;
 
-    samples_per_frame = mgba_core_get_sample_rate (HS_CORE (self)) / mgba_core_get_frame_rate (HS_CORE (self));
+  samples_per_frame = mgba_core_get_sample_rate (HS_CORE (self)) / mgba_core_get_frame_rate (HS_CORE (self));
 
-    self->audio_buffer_size = samples_per_frame * 2;
-    self->audio_buffer = malloc (self->audio_buffer_size * sizeof(int16_t));
-    self->audio_samples_per_frame_avg = (float) samples_per_frame;
+  self->audio_buffer_size = samples_per_frame * 2;
+  self->audio_buffer = malloc (self->audio_buffer_size * sizeof(int16_t));
+  self->audio_samples_per_frame_avg = (float) samples_per_frame;
 
-    /* Internal audio buffer size should be
-     * audioSamplesPerFrame, but number of samples
-     * actually generated varies slightly on a
-     * frame-by-frame basis. We therefore allow
-     * for some wriggle room by setting double
-     * what we need (accounting for the hard
-     * coded blip buffer limit of 0x4000). */
-    self->core->setAudioBufferSize (self->core, MIN (samples_per_frame * 2, 0x4000));
-  } else {
-    self->stream.videoDimensionsChanged = 0;
-    self->stream.postAudioFrame = 0;
-    self->stream.postAudioBuffer = postAudioBuffer;
-    self->stream.postVideoFrame = 0;
-
-    self->audio_buffer_size = GB_SAMPLES * 2;
-    self->audio_buffer = malloc (GB_SAMPLES * 2 * sizeof(int16_t));
-    self->audio_samples_per_frame_avg = GB_SAMPLES;
-
-    self->core->setAVStream (self->core, &self->stream);
-    self->core->setAudioBufferSize (self->core, GB_SAMPLES);
-  }
+  /* Internal audio buffer size should be
+   * audioSamplesPerFrame, but number of samples
+   * actually generated varies slightly on a
+   * frame-by-frame basis. We therefore allow
+   * for some wriggle room by setting double
+   * what we need (accounting for the hard
+   * coded blip buffer limit of 0x4000). */
+  self->core->setAudioBufferSize (self->core, MIN (samples_per_frame * 2, 0x4000));
 
   self->core->setPeripheral (self->core, mPERIPH_RUMBLE, &self->rumble);
 }
@@ -485,8 +368,6 @@ mgba_core_class_init (mGBACoreClass *klass)
   core_class->get_aspect_ratio = mgba_core_get_aspect_ratio;
 
   core_class->get_sample_rate = mgba_core_get_sample_rate;
-
-  core_class->get_region = mgba_core_get_region;
 }
 
 static void
@@ -495,77 +376,6 @@ mgba_core_init (mGBACore *self)
   g_assert (!core);
 
   core = self;
-}
-
-static void
-mgba_game_boy_core_set_model (HsGameBoyCore *core, HsGameBoyModel model)
-{
-  mGBACore *self = MGBA_CORE (core);
-  enum GBModel mgba_model;
-  const char *model_name;
-
-  switch (model) {
-  case HS_GAME_BOY_MODEL_GAME_BOY:
-    mgba_model = GB_MODEL_DMG;
-    break;
-  case HS_GAME_BOY_MODEL_GAME_BOY_POCKET:
-    mgba_model = GB_MODEL_MGB;
-    break;
-  case HS_GAME_BOY_MODEL_GAME_BOY_COLOR:
-    mgba_model = GB_MODEL_CGB;
-    break;
-  case HS_GAME_BOY_MODEL_GAME_BOY_ADVANCE:
-    mgba_model = GB_MODEL_AGB;
-    break;
-  case HS_GAME_BOY_MODEL_SUPER_GAME_BOY:
-    mgba_model = GB_MODEL_SGB;
-    break;
-  case HS_GAME_BOY_MODEL_SUPER_GAME_BOY_2:
-    mgba_model = GB_MODEL_SGB2;
-    break;
-  default:
-    g_assert_not_reached ();
-  }
-
-  model_name = GBModelToName (mgba_model);
-
-  self->gb_model = mgba_model;
-
-  mCoreConfigSetDefaultValue (&self->core->config, "gb.model", model_name);
-  mCoreConfigSetDefaultValue (&self->core->config, "sgb.model", model_name);
-  mCoreConfigSetDefaultValue (&self->core->config, "cgb.model", model_name);
-  mCoreConfigSetDefaultValue (&self->core->config, "cgb.hybridModel", model_name);
-  mCoreConfigSetDefaultValue (&self->core->config, "cgb.sgbModel", model_name);
-}
-
-static void
-mgba_game_boy_core_set_palette (HsGameBoyCore *core, int *colors, int n_colors)
-{
-  mGBACore *self = MGBA_CORE (core);
-
-  g_assert (n_colors == 4 || n_colors == 12);
-
-  mCoreConfigSetUIntValue (&self->core->config, "gb.pal[0]",  colors[0  % n_colors]);
-  mCoreConfigSetUIntValue (&self->core->config, "gb.pal[1]",  colors[1  % n_colors]);
-  mCoreConfigSetUIntValue (&self->core->config, "gb.pal[2]",  colors[2  % n_colors]);
-  mCoreConfigSetUIntValue (&self->core->config, "gb.pal[3]",  colors[3  % n_colors]);
-  mCoreConfigSetUIntValue (&self->core->config, "gb.pal[4]",  colors[4  % n_colors]);
-  mCoreConfigSetUIntValue (&self->core->config, "gb.pal[5]",  colors[5  % n_colors]);
-  mCoreConfigSetUIntValue (&self->core->config, "gb.pal[6]",  colors[6  % n_colors]);
-  mCoreConfigSetUIntValue (&self->core->config, "gb.pal[7]",  colors[7  % n_colors]);
-  mCoreConfigSetUIntValue (&self->core->config, "gb.pal[8]",  colors[8  % n_colors]);
-  mCoreConfigSetUIntValue (&self->core->config, "gb.pal[9]",  colors[9  % n_colors]);
-  mCoreConfigSetUIntValue (&self->core->config, "gb.pal[10]", colors[10 % n_colors]);
-  mCoreConfigSetUIntValue (&self->core->config, "gb.pal[11]", colors[11 % n_colors]);
-
-  self->core->reloadConfigOption (self->core, "gb.pal", NULL);
-}
-
-static void
-mgba_game_boy_core_init (HsGameBoyCoreInterface *iface)
-{
-  iface->set_model = mgba_game_boy_core_set_model;
-  iface->set_palette = mgba_game_boy_core_set_palette;
 }
 
 static void
